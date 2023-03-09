7e6d29342e9708e11fa8f92683fc91eac8a930f3
matth747 <109696245+matth747@users.noreply.github.com>
2023-03-09 10:48:25 -0600

    added client server structure with working user

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h6>
+                Footer
+            </h6>
+        </footer>
+    )
+}
+
+export default Footer;
